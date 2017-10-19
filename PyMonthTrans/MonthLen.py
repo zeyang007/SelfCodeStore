931350d4fca7420e456b872e03edeca483fdc587
zeyang007 <247833821@qq.com>
2017-10-19 23:04:25 +0800

    Create MonthLen.py

---
+++ PyMonthTrans/MonthLen.py
@@ -0,0 +1,39 @@
+Jan = "January"
+Feb = "February"
+Mar = "March"
+Apr = "April"
+May = "May"
+Jun = "June"
+Jul = "July"
+Aug = "August"
+Sep = "September"
+Oct = "October"
+Nov = "November"
+Dec = "December"
+
+def getMonthLength(month):
+    if(month == 1):
+        return len(Jan)
+    elif(month == 2):
+        return len(Feb)
+    elif(month == 3):
+        return len(Mar)
+    elif(month == 4):
+        return len(Apr)
+    elif(month == 5):
+        return len(May)
+    elif(month == 6):
+        return len(Jun)
+    elif(month == 7):
+        return len(Jul)
+    elif(month == 8):
+        return len(Aug)
+    elif(month == 9):
+        return len(Sep)
+    elif(month == 10):
+        return len(Oct)
+    elif(month == 11):
+        return len(Nov)
+    elif(month == 12):
+        return len(Dec)
+
